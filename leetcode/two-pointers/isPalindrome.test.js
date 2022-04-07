52d186d63652b8a824a814cde65469b6c829ec64
Blossom <saphashb@gmail.com>
2022-04-07 11:42:07 +0100

    valid palindrome

---
+++ leetcode/two-pointers/isPalindrome.test.js
@@ -0,0 +1,24 @@
+describe('is valid palindrome', () => {
+  it('returns true if phrase is a valid palindrome', () => {
+    expect(isPalindrome("Don't nod")).toBeDefined()
+    expect(isPalindrome('I did, did I?')).toBeDefined()
+    expect(isPalindrome('Amore roma')).toBeDefined()
+  })
+  it('returns false if phrase is not a valid palindrome', () => {
+    expect(isPalindrome('race a car')).toBeFalsy()
+    expect(isPalindrome('Good job')).toBeFalsy()
+  })
+})
+
+const isPalindrome = s => {
+  s = s.replace(/[^a-zA-Z0-9]/g).toLowerCase()
+
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    if (s[left++] !== s[right--]) return false
+  }
+
+  return true
+}
