f65a55a64523ee5cd1f223edef422feb9df115a2
Blossom <saphashb@gmail.com>
2022-04-16 17:09:04 +0100

    use ord library to replace charcodeAt

--- meta/rotational-ciphers.test.js
+++ meta/rotational-ciphers.test.js
@@ -6,44 +6,41 @@ https://www.facebookrecruiting.com/portal/coding_practice_question/?problem_id=2
 0(n) - time | 0(1) - space
  */
 
+const { ord, ordFrom } = require('js-ord')
+
 describe('rotational ciphers', () => {
   it('encrypts a string by return the third value of every string and number', () => {
-  expect(rotationalCipher("Zebra-493?", 3)).toBe("Cheud-726?")
-  expect(rotationalCipher("abcdefghijklmNOPQRSTUVWXYZ0123456789", 39)).toBe("nopqrstuvwxyzABCDEFGHIJKLM9012345678")
+    expect(rotationalCipher("Zebra-493?", 3)).toBe("Cheud-726?")
+    expect(rotationalCipher("abcdefghijklmNOPQRSTUVWXYZ0123456789", 39)).toBe("nopqrstuvwxyzABCDEFGHIJKLM9012345678")
   })
 })
 
 
 
 const rotationalCipher = (string, rotation) => {
-let cipher = []
+  let cipher = []
+  for (let str of string) {
+    let validChar = str.match(/[a-zA-Z0-9]/g)
 
-for (let str of string){
-  let validChar = str.match(/[a-zA-Z0-9]/g)
-
-  if (str === '0'){
-    cipher.push(9)
+    if (str === '0') {
+      cipher.push(9)
+    }
+    else if (Number(str)) {
+      str = (Number(str) + rotation) % 10
+      cipher.push(str)
+    }
+    else if (validChar && str === str.toUpperCase()) {
+      str = ((ord(str) - ord('A')) + rotation) % 26
+      cipher.push(ordFrom(ord('A') + str))
+    }
+    else if (validChar && str === str.toLowerCase()) {
+      str = ((ord(str) - ord('a')) + rotation) % 26
+      cipher.push(ordFrom(ord('a') + str))
+    }
+    else {
+      cipher.push(str)
+    }
   }
 
-  else if(Number(str)){
-    str = (Number(str) + rotation) % 10
-    cipher.push(str)
-  }
-
-  else if(validChar && str === str.toUpperCase()){
-    str = ((str.charCodeAt() - 'A'.charCodeAt()) + rotation) % 26
-    cipher.push(String.fromCharCode('A'.charCodeAt() + str))
-  }
-
-  else if(validChar && str === str.toLowerCase()){
-    str = ((str.charCodeAt() - 'a'.charCodeAt()) + rotation) % 26
-    cipher.push(String.fromCharCode('a'.charCodeAt() + str))
-  }
-
-  else{
-    cipher.push(str)
-  }
-}
-
-return cipher.join('')
+  return cipher.join('')
 }
