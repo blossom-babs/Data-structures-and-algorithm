008a38744e3592c49488bd0eee6f40a2af5530e1
Blossom <saphashb@gmail.com>
2022-04-16 17:15:47 +0100

    leetcode/arrays: group anagrams

--- leetcode/arrays/group-anagrams.test.js
+++ leetcode/arrays/group-anagrams.test.js
@@ -2,12 +2,29 @@
 
 Test: npm test ./leetcode/arrays/group-anagrams.test.js
 
+0(n) - space | 0(m * nlogn) - time
  */
 
 describe('Group Anagrams', () => {
   it('groups anagrams together', () => {
-    expect (groupAnagrams(["eat","tea","tan","ate","nat","bat"])).toBe([["bat"],["nat","tan"],["ate","eat","tea"]])
-    expect (groupAnagrams([" "])).toBe([[[" "]]])
-    expect (groupAnagrams(["a"])).toBe([[["a"]]])
+    expect(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])).toStrictEqual([["eat", "tea", "ate"], ["tan", "nat"], ["bat"]])
+    expect(groupAnagrams([" "])).toStrictEqual([[" "]])
+    expect(groupAnagrams(["a"])).toStrictEqual([["a"]])
   })
 })
+
+const groupAnagrams = strs => {
+  let hash = {}
+
+  for (let str of strs) {
+    let sorted = str.split('').sort().join('')
+
+    if (hash[sorted]) {
+      hash[sorted].push(str)
+    } else {
+      hash[sorted] = [str]
+    }
+  }
+
+  return Object.values(hash)
+}
