21730346b382ae62ce48763800c294e42f5ee814
Blossom <saphashb@gmail.com>
2022-05-20 04:28:05 +0100

    leetcode: contains duplicates

--- leetcode/easy/contains-duplicates.test.js
+++ leetcode/easy/contains-duplicates.test.js
@@ -1,44 +1,39 @@
-/* https://leetcode.com/problems/contains-duplicate/
+// https://leetcode.com/problems/contains-duplicate/
+//Test: npm test ./leetcode/easy/contains-duplicates.test.js
 
-Test: npm test ./leetcode/easy/contains-duplicates.test.js
-
-complexity analysis
-0(n) - time
-0(1) - space
- */
 
 describe('check if arr of integers contains duplicates', () => {
   it('returns true if duplicate value is found', () => {
     expect(containsDuplicate([1, 2, 3, 1])).toBe(true)
-    expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true)
+    expect(containsDuplicateTwo([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true)
+    expect(containsDuplicateThree([-1, -1, 1, 2, 3, 4, 5, 5, 1])).toBe(true)
   })
   it('returns false if duplicate not found', () => {
     expect(containsDuplicate([1, 2, 3, 4])).toBe(false)
+    expect(containsDuplicateTwo([3, 4])).toBe(false)
+    expect(containsDuplicateThree([])).toBe(false)
   })
 })
 
+// April 2022
+// 0(n log n) - time | 0(log n) - space (quick sort)
 const containsDuplicate = nums => {
-  nums = nums.sort((a,b) => a -b)
+  nums = nums.sort((a, b) => a - b)
 
-  for(let i = 0; i<nums.length; i++){
-    if(nums[i] === nums[i + 1]) return true
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) return true
   }
 
   return false
 }
 
-/* 
-A different approach
-
-0(n) - time
-0(n) - space
-
-const containsDuplicate = nums => {
+//0(n) - time | 0(n) - space
+const containsDuplicateTwo = nums => {
   let numsTable = {}
   let i = 0
 
-  while(i < nums.length){
-    if((nums[i] in numsTable)){
+  while (i < nums.length) {
+    if ((nums[i] in numsTable)) {
       return true
     }
     numsTable[nums[i]] = i
@@ -47,4 +42,15 @@ const containsDuplicate = nums => {
   return false
 }
 
-*/
+// May 20 2022 | 4am wat
+// 0(n) - time | 0(n) - space
+
+const containsDuplicateThree = nums => {
+  let hash = {}
+
+  for (const num of nums) {
+    if (hash[num]) return true
+    hash[num] = 1
+  }
+  return false
+}
