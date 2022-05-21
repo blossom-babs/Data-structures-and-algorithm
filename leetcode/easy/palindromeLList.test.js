0f8687b50c7dae8e801ffebc54d004d634765186
Blossom <saphashb@gmail.com>
2022-05-21 16:46:52 +0100

    more efficient time complexity

--- leetcode/easy/palindromeLList.test.js
+++ leetcode/easy/palindromeLList.test.js
@@ -1,21 +1,20 @@
 // https://leetcode.com/problems/palindrome-linked-list/
 // Test: npm test ./leetcode/easy/palindromeLList.test.js
-//0(n^2) - time | 0(n) - space
 
 describe('#Palindrome linked list', () => {
   let llist;
-
+  
   beforeEach(() => {
     llist = new singlyLList()
   })
-
+  
   it('returns true if linked list is a palindrome ', () => {
     llist.insert(1); llist.insert(2); llist.insert(2); llist.insert(1);
-    expect(isPalindrome(llist.root)).toBe(true)
+    expect(isPalindromeTwo(llist.root)).toBe(true)
   })
   it('returns false if linked list not a palindrome', () => {
     llist.insert(1); llist.insert(2);
-    expect(isPalindrome(llist.root)).toBe(false)
+    expect(isPalindromeTwo(llist.root)).toBe(false)
   })
 })
 
@@ -30,37 +29,63 @@ class singlyLList {
   constructor() {
     this.root = null
   }
-
+  
   insert(data) {
     let nnode = new LNode(data)
     if (!this.root) {
       this.root = nnode
       return this.root
     }
-
+    
     let curr = this.root
     while (curr.next) {
       curr = curr.next
     }
-
+    
     curr.next = nnode
     return curr
   }
 }
 
+//0(n) - time | 0(1) - space
+const isPalindromeTwo = head => {
+  let [fast, slow] = [head, head]
+  while(fast && fast.next){
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  let [curr, prev] = [slow, null]
+  while(curr){
+    const nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  }
+
+  let[left, right] = [head, prev]
+  while(right){
+    if(right.val !== left.val) return false
+    right = right.next
+    left = left.next
+  }
+  return true
+}
+
+//0(n) - time | 0(n) - space
 const isPalindrome = head => {
   let curr = head
   let values = []
-
+  
   while (curr) {
     values.push(curr.val)
     curr = curr.next
   }
-
+  
   if (!curr && values.length > 0) {
     let l = 0
     let r = values.length - 1
-
+    
     while (l < r) {
       if (values[l] !== values[r]) return false
       l++;
@@ -70,4 +95,3 @@ const isPalindrome = head => {
   }
   return true
 }
-
