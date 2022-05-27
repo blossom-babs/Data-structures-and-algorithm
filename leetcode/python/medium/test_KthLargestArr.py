02a03a80488f39b47ac146db07a60d9e379bc6f8
Blossom <saphashb@gmail.com>
2022-05-27 14:38:47 +0100

    fix spacing

--- leetcode/python/medium/test_KthLargestArr.py
+++ leetcode/python/medium/test_KthLargestArr.py
@@ -5,24 +5,20 @@ import unittest
 """
 https://leetcode.com/problems/kth-largest-element-in-an-array/
 """
-
-
 class Solution:
     """
     sort method
     0(n log n) - TC | 0(n) - SC
     """
-
     def KthLargestArr(self, nums: List[int], k: int) -> int:
         nums.sort()
         idx = len(nums) - k
         return nums[idx]
 
     """
-  Heap method
-  0(k log n) - TC | 0(1) - SC
-  """
-
+    Heap method
+    0(k log n) - TC | 0(1) - SC
+    """
     def KthLargestHeap(self, nums: List[int], k: int) -> int:
         heapq.heapify(nums)
         while len(nums) > k:
