d17b0bcca262b929cf2711a2f5ba48c51c461621
Blossom <saphashb@gmail.com>
2022-06-10 17:04:24 +0100

    lengthOfLongestSubstring

--- leetcode/python/medium/lengthOfLongestSubstring.py
+++ leetcode/python/medium/lengthOfLongestSubstring.py
@@ -17,6 +17,22 @@ def lengthOfLongestSubstring(s:str) -> int:
 
   return res
 
+#solution 2
+
+def lengthOfLongestSubstring(s):   
+    res, count, hash = 0, 0, set()
+        
+    for i in range(len(s)):
+        while s[i] in hash:
+            hash.pop()
+            count -= 1
+                
+        hash.add(s[i])
+        count += 1
+        res = max(count, res)
+            
+    return res
+
 class Test(unittest.TestCase):
   def test_solution(self):
     self.assertEqual(lengthOfLongestSubstring(""), 0)
