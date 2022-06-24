8e99a97be1abc96789102b4b87bc52c576101292
Blossom <saphashb@gmail.com>
2022-06-24 04:48:31 +0100

    javascript folder

--- data-structures/graphs/connectCompCount.py
+++ data-structures/graphs/connectCompCount.py
@@ -18,3 +18,32 @@ connectedComponentsCount({
   4: [3, 2]
 }) # 2
 '''
+cc = {
+    0: [8, 1, 5],
+    1: [0],
+    5: [0, 8],
+    8: [0, 5],
+    2: [3, 4],
+    3: [2, 4],
+    4: [3, 2]
+}  # 2
+
+
+def connectedComponentsCount(graph):
+    count, visited = 0, set()
+    for key in graph:
+        if dfs(graph, key, visited) == True:
+          count += 1
+    return count
+
+
+def dfs(graph, key, visited):
+    if key in visited:
+        return False
+    visited.add(key)
+    for neighbor in graph[key]:
+        dfs(graph, neighbor, visited)
+    return True
+
+
+print(connectedComponentsCount(cc))
