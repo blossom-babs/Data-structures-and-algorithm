8558df1ce0a3b684f79fe731fbc3f5e4e8b50a27
Blossom <saphashb@gmail.com>
2022-07-01 18:09:42 +0100

    recurse climb stairs solution

--- leetcode/python/easy/climbStairs.py
+++ leetcode/python/easy/climbStairs.py
@@ -15,12 +15,28 @@ def climbStairs(n) -> int:
 
   return one
 
-
+def climbStairsRecursive(n) -> int:
+  memo = {}
+  
+  def recurse(n):
+    if n in memo:
+      return memo[n]
+    if n == 1:
+      return 1
+    if n == 2:
+      return 2
+    memo[n] = recurse(n - 1) + recurse(n - 2)
+    return memo[n]
+  
+  return recurse(n)
+    
+print(climbStairsRecursive(38))
 class Test(unittest.TestCase):
   def test_climbStairs(self):
-    self.assertEqual(climbStairs(2), 2)
-    self.assertEqual(climbStairs(3), 3)
-    self.assertEqual(climbStairs(5), 8)
+    self.assertEqual(climbStairsRecursive(2), 2)
+    self.assertEqual(climbStairsRecursive(3), 3)
+    self.assertEqual(climbStairsRecursive(5), 8)
+    self.assertEqual(climbStairsRecursive(38), 63245986)
 
 
 if __name__ == "__main__":
