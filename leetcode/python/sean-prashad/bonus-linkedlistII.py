d35befb67ec4339170c8403b468f6d82efeea2c7
Blossom <saphashb@gmail.com>
2022-08-02 10:23:25 +0100

    linked list cycle II https://leetcode.com/problems/linked-list-cycle-ii/submissions/

--- leetcode/python/sean-prashad/bonus-linkedlistII.py
+++ leetcode/python/sean-prashad/bonus-linkedlistII.py
@@ -1,18 +1,47 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+'''
+https://leetcode.com/problems/linked-list-cycle-ii/submissions/
+'''
+
+# set solution
+# 0(n) - tc | 0(n) - sc
+
+
 class Solution:
-  def findCycle(head):
-    slow = fast = head
-    while fast and fast.next:
-      slow = slow.next
-      fast = fast.next.next
-      
-      if slow == fast:
-        break
-      return None
-      
-    pointer = head
-    
-    while pointer:
-      if pointer == fast:
-        return pointer
-      pointer = pointer.next
-      fast = fast.next
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        hash = set()
+        current = head
+
+        while current:
+            if current in hash:
+                return current
+            hash.add(current)
+            current = current.next
+
+        return None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = pointer = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if fast == slow:
+                while pointer != fast:
+                    pointer = pointer.next
+                    fast = fast.next
+                return pointer
+
+        return None
