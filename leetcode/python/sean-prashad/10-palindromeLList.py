443264dd6b100ed34e5c9ca7b2acbd0b41a46d9c
Blossom <saphashb@gmail.com>
2022-08-03 09:12:08 +0100

    optimal palindrome linked list https://leetcode.com/problems/palindrome-linked-list/

--- leetcode/python/sean-prashad/10-palindromeLList.py
+++ leetcode/python/sean-prashad/10-palindromeLList.py
@@ -24,3 +24,33 @@ class Solution:
             left += 1
             right -= 1
         return True
+      
+
+# 0(n) - tc | 0(n) - sc
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow, fast = head, head
+        
+        #find the middle of the linked list
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        # reverse the middle of the linked list from the slow pointer
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+            
+        # find palindrom
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+            
+        return True
+        
