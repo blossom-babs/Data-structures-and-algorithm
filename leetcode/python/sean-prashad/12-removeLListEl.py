f1344ee383d3d5dbe8ddb7d7835708929c0cede2
Blossom <saphashb@gmail.com>
2022-08-09 08:56:14 +0100

    https://leetcode.com/problems/remove-linked-list-elements/

--- leetcode/python/sean-prashad/12-removeLListEl.py
+++ leetcode/python/sean-prashad/12-removeLListEl.py
@@ -26,4 +26,25 @@ class Solution:
             curr = nxt
         return dummy.next
         
+
+# recursive solution - 0(n) - TC | 0(n) - sc
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if head is None: return None
+        nxt = self.removeElements(head.next, val)
+        if head.val == val:
+            head = nxt
+        else:
+            head.next = nxt
+        return head
+    
+    # solution 1 pointer - 0(n) - tc | 0(1) - sc
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = cur = ListNode()
+        dummy.next = head
+        while cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy.next
         
