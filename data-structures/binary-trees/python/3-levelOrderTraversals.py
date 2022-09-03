9f37a91a6b142198c7c9b65158280233db1b4df3
Blossom <saphashb@gmail.com>
2022-09-03 19:47:00 +0100

    level order traversals

---
+++ data-structures/binary-trees/python/3-levelOrderTraversals.py
@@ -0,0 +1,65 @@
+# insert value into given Node
+
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data)
+        if self.right:
+            self.right.printTree()
+
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        return res
+
+    def PreorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PreorderTraversal(root.left)
+            res = res + self.PreorderTraversal(root.right)
+        return res
+
+    def PostorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.PostorderTraversal(root.left)
+            res = res + self.PostorderTraversal(root.right)
+            res.append(root.data)
+        return res
+
+
+root = Node(10)
+root.insert(2)
+root.insert(15)
+root.insert(8)
+root.insert(5)
+root.insert(12)
+root.printTree()
+print(root.inorderTraversal(root)) # left -> root -> right 2, 5, 8, 10, 12, 15
+print(root.PreorderTraversal(root)) # root -> left -> right 10, 2, 8, 5, 15, 12
+print(root.PostorderTraversal(root)) # left -> right -> root 5, 8, 2, 12, 15, 10
