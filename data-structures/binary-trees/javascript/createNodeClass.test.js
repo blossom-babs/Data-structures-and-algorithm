6e000dd8b323fb9599ce71d2265ae54f287daa2f
Blossom <saphashb@gmail.com>
2022-09-05 14:05:16 +0100

    javascript create node class equivalent

---
+++ data-structures/binary-trees/javascript/createNodeClass.test.js
@@ -0,0 +1,12 @@
+// create a Node class
+
+class Node{
+  constructor(data){
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+}
+
+let root = new Node(10)
+console.log(root)
--- data-structures/binary-trees/python/1-createNodeClass.py
+++ data-structures/binary-trees/python/1-createNodeClass.py
@@ -7,19 +7,7 @@ class Node:
         self.right = None
         
     def printTree(self):
-        print(self.data)
-      
-'''
-              10
-           /     \
-        67         2
-       /
-    87
-'''  
-      
-root = Node(10)
-root.right = Node(2)
-root.left = Node(67)
-root.left.left = Node(87)
-root.left.left.printTree()
+        print(self)
 
+root = Node(10)
+root.printTree()
