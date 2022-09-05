e28523b339e5431f4a14389d07acda98a30f5f50
Blossom <saphashb@gmail.com>
2022-09-05 13:55:49 +0100

    script to run python code from package.json

--- DailyCodingProblem/readme.md
+++
@@ -1,5 +0,0 @@
-# Daily Coding Problem
-
-Daily coding Problem was founded by Marc with the intent to send his friends a problem a day to help them remain consistent and build their problem solvin skills.
-
-Subscribe to this [mailing list](https://www.dailycodingproblem.com) and you will get problems of varying difficulty to work on each day.
--- data-structures/binary-trees/python/1-createNodeClass.py
+++ data-structures/binary-trees/python/1-createNodeClass.py
@@ -1,13 +1,25 @@
 # create a python node
 
 class Node:
-    def __init__(self, data, left=None, right=None):
+    def __init__(self, data):
         self.data = data
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
         
     def printTree(self):
         print(self.data)
-        
+      
+'''
+              10
+           /     \
+        67         2
+       /
+    87
+'''  
+      
 root = Node(10)
-root.printTree()
+root.right = Node(2)
+root.left = Node(67)
+root.left.left = Node(87)
+root.left.left.printTree()
+
